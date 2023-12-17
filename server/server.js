5b8be5b28ad3a284006ffa7a8707d5ec2a26889f
dejwodejo <d.drapinski@proton.me>
2023-12-17 14:03:54 +0100

    add cors middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import mysqlRouter from "./mysqlRoute.js";
 import influxRouter from "./influxRoute.js";
 import env from './envVariables.js'
 
 const server = express();
+server.use(cors({methods: 'GET'}))
 
 server.use('/api/sensors', influxRouter);
 server.use('/api', mysqlRouter);
