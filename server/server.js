5c05f5a2195822392b61c62a518a3d2b3c01dfea
dejwodejo <d.drapinski@proton.me>
2023-12-27 20:41:16 +0100

    sensors data moved to mysql

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import mysqlRouter from "./mysqlRoute.js";
-import influxRouter from "./influxRoute.js";
 import env from './envVariables.js'
 
 const server = express();
 server.use(cors({methods: 'GET'}))
 
-server.use('/api/sensors', influxRouter);
 server.use('/api', mysqlRouter);
 
 server.listen(env.PROXY_SERVER_PORT, () => console.log(`App running on port ${env.PROXY_SERVER_PORT}`))
