efbb0c930e6f800c4b4778c493b2ac24755608e8
dejwodejo <d.drapinski@proton.me>
2023-12-28 14:46:49 +0100

    naming change after db structure change

--- server/databaseRouter.js
+++ server/databaseRouter.js
@@ -28,8 +28,8 @@ mysqlRouter.get(`/simulation`, async (req, res) => {
 })
 
 mysqlRouter.get('/sensors/air-pollution', async (req, res) => {
-    const param = req.query.param;
-    if (!isValidMeasurementParam(param)) return res.status(400).json({error: 'invalid measurement value'});
+    const sensorId = req.query.sensorId;
+    if (!isValidSensorId(sensorId)) return res.status(400).json({error: 'invalid sensor value'});
 
     const tableName = 'ttsensors_value';
     const mysqlConnection = mysql.createConnection({
@@ -40,23 +40,25 @@ mysqlRouter.get('/sensors/air-pollution', async (req, res) => {
         port: env.MYSQL_PORT
     })
 
-    mysqlConnection.query(`SELECT *
-                           FROM ${tableName}
-                           WHERE measurement = '${param}'`, (err, result) => {
+    const sqlQuery = sensorId ? `SELECT *
+                                 FROM ${tableName}
+                                 WHERE id = "${sensorId}"
+                                   AND datetime >= NOW() - INTERVAL 1 DAY;` : `SELECT *
+                                                                               FROM ${tableName}
+                                                                               WHERE datetime >= NOW() - INTERVAL 1 DAY;`;
+
+    mysqlConnection.query(sqlQuery, (err, result) => {
         if (err) {
             console.error(err);
             res.status(404).json({error: 'internal server error'});
         } else
             return res.send(result)
     })
-
 })
 
 mysqlRouter.get('/api/sensors/meteo', (req, res) => {
     const param = req.query.param;
     if (!isValidMeteoMeasurementParam(param)) return res.status(400).json({error: 'invalid measurement value'});
-    const sensorId = req.query.sensorId;
-    if (!isValidSensorId(sensorId)) return res.status(400).json({error: 'invalid measurement value'});
 
     const tableName = 'ttmeteo_value';
     const mysqlConnection = mysql.createConnection({
@@ -67,13 +69,10 @@ mysqlRouter.get('/api/sensors/meteo', (req, res) => {
         port: env.MYSQL_PORT
     })
 
-    const sqlQuery = sensorId ? `SELECT *
-                                 FROM ${tableName}
-                                 WHERE measurement = '${param}'
-                                   AND datetime >= NOW() - INTERVAL 1 DAY;` : `SELECT *
-                                                                               FROM ${tableName}
-                                                                               WHERE measurement = '${param}'
-                                                                                 AND datetime >= NOW() - INTERVAL 1 DAY;`;
+    const sqlQuery = `SELECT *
+                      FROM ${tableName}
+                      WHERE measurement = '${param}'
+                        AND datetime >= NOW() - INTERVAL 1 DAY;`;
 
     mysqlConnection.query(sqlQuery, (err, result) => {
         if (err) {
@@ -85,7 +84,7 @@ mysqlRouter.get('/api/sensors/meteo', (req, res) => {
 })
 
 function isValidMeasurementParam(param) {
-    const validMeasurements = ['CO', 'NO2', 'O3', 'PM10', 'PM25', 'SO2'];
+    const validMeasurements = ['CO', 'NO2', 'O3', 'PM10', 'PM2.5', 'SO2'];
     return validMeasurements.includes(param);
 }
 
@@ -95,7 +94,7 @@ function isValidMeteoMeasurementParam(param) {
 }
 
 function isValidSensorId(param) {
-    const validAirPollutionSensors = ['reference', 'be7a0000000029f7', 'be7a0000000029fc', 'be7a0000000029fd', 'be7a0000000029fe', 'be7a0000000029f2', 'be7a0000000029f9', 'be7a0000000029fa', 'be7a0000000029fb', 'be7a0000000029f5', 'be7a0000000029f6', 'be7a0000000029f1', 'be7a0000000029f8', 'be7a0000000029f3', 'be7a0000000029f4']
+    const validAirPollutionSensors = [undefined, 'reference', 'be7a0000000029f7', 'be7a0000000029fc', 'be7a0000000029fd', 'be7a0000000029fe', 'be7a0000000029f2', 'be7a0000000029f9', 'be7a0000000029fa', 'be7a0000000029fb', 'be7a0000000029f5', 'be7a0000000029f6', 'be7a0000000029f1', 'be7a0000000029f8', 'be7a0000000029f3', 'be7a0000000029f4']
     return validAirPollutionSensors.includes(param);
 }
 
--- server/geoJsonMapping/findProperThreshold.js
+++
@@ -1,5 +0,0 @@
-import ColorThresholdsIndex from "./colorThresholdsIndex.js";
-
-export default function findProperThreshold(parameter) {
-    return ColorThresholdsIndex.find(({name}) => name === parameter).colorThresholds;
-}
