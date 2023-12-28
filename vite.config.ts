0ac15e6b6e3a2b3fa2d2bd9b09249bdcf2c751a9
dejwodejo <d.drapinski@proton.me>
2023-12-28 15:58:04 +0100

    fix cors error

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig( {
+    server: {
+        proxy: {
+            "/api": "http://localhost:5000",
+        }
+    },
+    plugins: [ react() ],
+} )
