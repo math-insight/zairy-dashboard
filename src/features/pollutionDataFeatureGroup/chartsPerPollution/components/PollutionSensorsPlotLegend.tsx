0137daf91f88b6573c85ddc2d4bf144e024e3ea2
dejwodejo <d.drapinski@proton.me>
2024-03-01 21:03:57 +0100

    add keys to map generated elements

--- src/features/pollutionDataFeatureGroup/chartsPerPollution/components/PollutionSensorsPlotLegend.tsx
+++ src/features/pollutionDataFeatureGroup/chartsPerPollution/components/PollutionSensorsPlotLegend.tsx
@@ -15,8 +15,8 @@ export default function PollutionSensorsPlotLegend( {
     return (
         <div className="legend-list-element-wrapper">
             { sensorsToPlot.map( ( { index, id, title, color, isActive } ) => (
-                <div className="legend-list-element">
-                    <Button color={ color } label={ `${ index }` }
+                <div key={ "plot-legend-wrapper" + index } className="legend-list-element">
+                    <Button key={ "plot-legend-sensor-button" + index } color={ color } label={ `${ index }` }
                             isActive={ isActive }
                             onClick={ () => onClick( id ) }/>
                     <p>{ title }</p>
