e82c1a2e8b7f711f347ec2cba37a206605aa1545
dejwodejo <d.drapinski@proton.me>
2024-06-05 15:17:03 +0200

    admin panel

---
+++ server/getSensorsPollutionsForCSV.js
@@ -0,0 +1,54 @@
+import {executeQuery} from "./databaseConnection.js";
+
+const MICROGRAMS_PER_MILLIGRAM = 1000;
+
+export default function getSensorsPollutionsForCSV(req, res, next) {
+    const fromDate = req.query.from;
+    const toDate = req.query.to;
+
+    const sqlQuery = `SELECT id, datetime, measurement, value, col
+                      FROM ttsensors_value
+                      WHERE datetime BETWEEN ? AND ?`;
+
+    executeQuery(sqlQuery, [fromDate, toDate], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({error: 'Internal server error'});
+        } else {
+            result.forEach(row => {
+                const {measurement} = row
+
+                if (measurement) {
+                    const sensor = sensorsDetails.find(sensor => sensor.id === row.id);
+
+
+                    if (sensor) {
+                        if (!sensor.data) {
+                            sensor.data = {};
+                        }
+                        if (!sensor.data[measurement]) {
+                            sensor.data[measurement] = [];
+                        }
+
+                        if (measurement === 'CO') {
+                            sensor.data[measurement].push({
+                                datetime: row.datetime,
+                                value: row.value / MICROGRAMS_PER_MILLIGRAM,
+                                color: row.col,
+                            })
+                        } else {
+                            sensor.data[measurement].push({
+                                datetime: row.datetime,
+                                value: row.value,
+                                color: row.col,
+                            })
+                        }
+
+                    }
+                }
+            })
+        }
+
+        next()
+    })
+}
