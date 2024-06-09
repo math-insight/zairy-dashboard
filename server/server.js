9d0235734ad260178eabd1592a33c4bdb62b6c2a
dejwodejo <d.drapinski@proton.me>
2024-06-09 12:14:39 +0200

    add endpoint for retrieving csv ready data

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import env from './envVariables.js'
+import express from "express";
+import cors from "cors";
+import env from "./envVariables.js";
 import getSensorsPollutionMeasurements from "./getSensorsPollutionMeasurements.js";
 import getSensorsDetails from "./getSensorsDetails.js";
 import getSensorsWeatherMeasurements from "./getSensorsWeatherMeasurements.js";
 import getSimulationPolygons from "./getSimulationPolygons.js";
 import getSimulationDatetime from "./getSimulationDatetime.js";
+import getSensorsPollutionsForCSV from "./getSensorsPollutionsForCSV.js";
 
 const server = express();
-server.use(cors({methods: 'GET'}));
+server.use(cors({ methods: "GET" }));
 
-server.get('/api/sensors', getSensorsDetails, getSensorsPollutionMeasurements, getSensorsWeatherMeasurements);
-server.get('/api/simulation', getSimulationPolygons)
-server.get('/api/simulation/datetime', getSimulationDatetime)
-server.listen(env.PROXY_SERVER_PORT, () => console.log(`App running on port ${env.PROXY_SERVER_PORT}`))
+server.get(
+  "/api/sensors",
+  getSensorsDetails,
+  getSensorsPollutionMeasurements,
+  getSensorsWeatherMeasurements
+);
+server.get("/api/sensors/csv", getSensorsDetails, getSensorsPollutionsForCSV);
+server.get("/api/simulation", getSimulationPolygons);
+server.get("/api/simulation/datetime", getSimulationDatetime);
+server.listen(env.PROXY_SERVER_PORT, () =>
+  console.log(`App running on port ${env.PROXY_SERVER_PORT}`)
+);
